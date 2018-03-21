61ffb46a7736c9cc26ae169f097cf2db193903a5
Serhii <radserhii@gmail.com>
2018-03-21 12:13:08 +0200

    Completed PagecController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,17 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Visiter;
+use App\Page;
+use Carbon\Carbon;
 
 class PageController extends Controller
 {
     public function index($id, Request $request)
     {
+        //Add visitor to db
         $visitor = new Visiter();
         $visitor->user_agent = $request->server('HTTP_USER_AGENT');
         $visitor->remote_adr = $request->server('REMOTE_ADDR');
         $visitor->http_referer = $request->server('HTTP_REFERER');
         $visitor->page_id = $id;
         $visitor->save();
+
+        //Add count yesterday, today and week of views pages to db
+        $yesterday = Carbon::yesterday();
+        $today = Carbon::today();
+        $week = Carbon::now()->subWeek();
+
+        $yesterday_cr = Visiter::where('page_id', $id)->whereBetween('created_at', [$yesterday, $today])->count();
+        $today_cr = Visiter::where('page_id', $id)->where('created_at', '>=', $today)->count();
+        $week_cr = Visiter::where('page_id', $id)->where('created_at', '>=', $week)->count();
+
+        $page = Page::find($id);
+
+        if(!$page) {
+            $page = new Page;
+            $page->id = $id;
+        };
+
+        $page->yesterday_cr = $yesterday_cr;
+        $page->today_cr = $today_cr;
+        $page->week_cr = $week_cr;
+        $page->save();
 
         return view('order_form', ['id' => $id]);
     }
--- app/Page.php
+++ app/Page.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
-    //
+    protected $fillable = ['page_number', 'yesterday_cr', 'today_cr', 'week_cr'];
+    public $incrementing = false;
 }
